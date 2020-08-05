b9f78b4f026209415b3c6f034406b8ef931e326c
adrian154 <94568a@gmail.com>
2020-08-05 14:46:54 -0700

    fixed missing dependency

--- static/scripts/editor.js
+++ static/scripts/editor.js
@@ -1,5 +1,6 @@
 let editor = CodeMirror(document.getElementById("topEditor"), {
     mode: "javascript",
+    theme: "material",
     lineNumbers: true,
     lineWrapping: true,
     matchBrackets: true,
