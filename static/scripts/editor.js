7e24f68717f6ad5df91d12ad1783a23bd4d708ef
adrian154 <94568a@gmail.com>
2020-08-05 23:37:53 -0700

    verification!!

--- static/scripts/editor.js
+++ static/scripts/editor.js
@@ -1,5 +1,8 @@
-let DOMEditor = document.getElementById("topEditor");
-let editor = CodeMirror(DOMEditor, {
+const resultsTable = document.getElementById("resultsTable");
+const consoleArea = document.getElementById("consoleArea");
+
+const DOMEditor = document.getElementById("topEditor");
+const editor = CodeMirror(DOMEditor, {
     mode: "javascript",
     theme: "material",
     lineNumbers: true,
@@ -14,19 +17,55 @@ let testCases = JSON.parse(challengeTestCasesJSON);
 
 const showResults = function(results) {
     console.log(results);
+
+    for(let rowNum = -1; rowNum < results.length; rowNum++) {
+        let row = resultsTable.insertRow();
+        for(let i = 0; i < 4; i++) {
+
+            let cell = row.insertCell();
+            let text;
+
+            if(rowNum == -1) {
+                switch(i) {
+                    case 0: text = "input data"; break;
+                    case 1: text = "expected"; break;
+                    case 2: text = "actual"; break;
+                    case 3: text = "verdict"; break;
+                }
+            } else {
+                cell.style.fontFamily = "Consolas, monospace";
+                let result = results[rowNum];
+                switch(i) {
+                    case 0: text = testCases[rowNum].params; break;
+                    case 1: text = testCases[rowNum].returnValue; break;
+                    case 2: text = result.error ? `error (${result.errorMessage})` : result.actual; break;
+                    case 3: text = ""; cell.style.backgroundColor = result.pass ? "#00ff00" : "#ff0000"; break;
+                }
+            }
+
+            cell.appendChild(document.createTextNode(text));
+
+        }
+    }
+
 };
 
 const getWorkerSource = function(code) {
     return `
+
+// Message handler: evaluate for test cases
 onmessage = (message) => postMessage(message.data.map(testCase => {
     let val;
     try {
         val = ${challengeName}(...testCase.params);
     } catch(error) {
-        return {pass: false, error: error.message};
+        return {pass: false, error: true, errorMessage: error.message};
     }
-    return {pass: val === testCase.returnValue, actual: val};
+    return {pass: val === testCase.returnValue, error: false, actual: val};
 }));
+
+// TODO: Implement fake "console" object
+
 ${code}
     `;
 };
@@ -49,7 +88,7 @@ const run = function() {
         worker.terminate();
 
         // Show results.
-        showResults(message);
+        showResults(message.data);
     
     };
 
