6386fae553418f683f4de7e6cf5c19eba2aafee6
adrian154 <94568a@gmail.com>
2020-08-05 22:39:11 -0700

    worked on semi sandboxed evaluation

--- static/scripts/editor.js
+++ static/scripts/editor.js
@@ -9,3 +9,48 @@ let editor = CodeMirror(DOMEditor, {
     indentUnit: 4,
     indentWithTabs: true
 });
+
+let testCases = JSON.parse(challengeTestCasesJSON);
+
+const showResults = function(results) {
+    console.log(results);
+};
+
+const getWorkerSource = function(code) {
+    return `
+onmessage = (message) => postMessage(message.data.map(testCase => {
+    let val;
+    try {
+        val = ${challengeName}(...testCase.params);
+    } catch(error) {
+        return {pass: false, error: error.message};
+    }
+    return {pass: val === testCase.returnVal, actual: val};
+}));
+${code}
+    `;
+};
+
+// Evaluate code in a web worker to "sandbox" it
+// Can still do XHR, etc. but the security risk is low (for now)
+const run = function() {
+
+    let code = getWorkerSource(editor.getValue());
+    console.log(code);
+    let worker = new Worker(URL.createObjectURL(new Blob([code], {type: "application/javascript"})));
+
+    // Run w/ test cases
+    console.log(testCases);
+    worker.postMessage(testCases);
+
+    worker.onmessage = (message) => {
+        
+        // This guy is done.
+        worker.terminate();
+
+        // Show results.
+        showResults(message);
+    
+    };
+
+};
