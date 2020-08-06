9d883c787adb5f05e8104479d2cb03c5710efd0c
adrian154 <94568a@gmail.com>
2020-08-06 12:07:45 -0700

    made tabs work

--- static/scripts/editor.js
+++ static/scripts/editor.js
@@ -1,5 +1,11 @@
 const resultsTable = document.getElementById("resultsTable");
 const consoleArea = document.getElementById("consoleArea");
+const runButton = document.getElementById("runButton");
+
+const tabResults = document.getElementById("tabResults");
+const tabConsole = document.getElementById("tabConsole");
+const resultsButton = document.getElementById("resultsButton");
+const consoleButton = document.getElementById("consoleButton");
 
 const DOMEditor = document.getElementById("topEditor");
 const editor = CodeMirror(DOMEditor, {
@@ -15,9 +21,19 @@ const editor = CodeMirror(DOMEditor, {
 
 let testCases = JSON.parse(challengeTestCasesJSON);
 
+const showResultsTab = function() {
+    tabResults.style.display = "block"; resultsButton.style.backgroundColor = "#888888";
+    tabConsole.style.display = "none"; consoleButton.style.backgroundColor = "#333333";
+};
+
+const showConsoleTab = function() {
+    tabResults.style.display = "none"; resultsButton.style.backgroundColor = "#333333";
+    tabConsole.style.display = "block"; consoleButton.style.backgroundColor = "#888888";
+};
+
 const showResults = function(results) {
 
-    //resultsTable.innerHTML = "";
+    resultsTable.innerHTML = "";
 
     for(let rowNum = -1; rowNum < results.length; rowNum++) {
         let row = resultsTable.insertRow();
@@ -71,26 +87,45 @@ ${code}
     `;
 };
 
+let running = false;
+
 // Evaluate code in a web worker to "sandbox" it
 // Can still do XHR, etc. but the security risk is low (for now)
 const run = function() {
 
+    // Don't re-run function if already evaluating
+    if(running)
+        return;
+    running = true;
+
+    // Create worker
     let code = getWorkerSource(editor.getValue());
-    console.log(code);
     let worker = new Worker(URL.createObjectURL(new Blob([code], {type: "application/javascript"})));
 
     // Run w/ test cases
-    console.log(testCases);
-    worker.postMessage(testCases);
+    // Small delay, so the user feels like something actually happened
+    setTimeout(function() {
+        console.log(testCases);
+        worker.postMessage(testCases);
+    }, 200);
+
+    // Update run button
+    runButton.innerHTML = "Running...";
+    runButton.disabled = true;
 
     worker.onmessage = (message) => {
         
-        // This guy is done.
-        worker.terminate();
+        // Restore run button
+        runButton.innerHTML = "&#9654; Run";
+        runButton.disabled = false;
 
-        // Show results.
+        // Finish up.
+        running = false;
+        worker.terminate();
         showResults(message.data);
     
     };
+
+    consoleArea.innerHTML = "console";
 
 };
